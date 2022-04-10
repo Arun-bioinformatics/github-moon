789c091e1b04797df80201e754536c696ec4847b
Arun Karnani Khemlani <arunkarnani@hotmail.com>
2022-04-10 22:23:05 +0100

    This is just me learning the basics of OOB

---
+++ classes.py
@@ -0,0 +1,25 @@
+import math
+
+class Person:
+        number_of_people = 0
+        gravity = -9.8
+        
+        def __init__(self, name):
+            self.name = name
+            Person.add_person()
+            
+        @classmethod
+        def number_of_people_(cls):
+            return cls.number_of_people
+        
+        @classmethod
+        def add_person(cls):
+            cls.number_of_people += 1
+            
+            
+p1 = Person("Tim")
+p2 = Person("Jill")
+
+print(Person.number_of_people_())
+
+print(p2.number_of_people)
